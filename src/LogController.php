fcbe32213936c85aa3f97ef5f9d4e885a4f01ed8
Vahid <vahidhasani09@gmail.com>
2020-10-29 16:01:59 +0330

    add log controller for found logs better

--- src/LogController.php
+++ src/LogController.php
@@ -4,7 +4,35 @@
 namespace Vahidid\ModelLogger;
 
 
+use Vahidid\ModelLogger\Models\LogModel;
+
 class LogController
 {
 
+    // GET BY MODEL NAMESPACE
+    public function getByModelNamespace(string $modelNamespace)
+    {
+        $logs = LogModel::query()
+            ->where('loggable_type', $modelNamespace)
+            ->get();
+
+        return $logs;
+    }
+
+    // GET BY MODEL NAMESPACE AND ID
+    public function getByModelNamepsaceAndId(string $modelNamespace, int $id)
+    {
+        $logs = LogModel::query()
+            ->where('loggable_type', $modelNamespace)
+            ->where('loggable_id', $id)
+            ->get();
+
+        return $logs;
+    }
+
+    // GET ALL LOGS
+    public function getAllLogs()
+    {
+        return LogModel::all();
+    }
 }
